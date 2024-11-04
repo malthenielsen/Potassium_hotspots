caf7e7446515b3f6b9875770e14262edc0a2d65b
malthe.nielsen <malthe.nielsen@sund.ku.dk>
2024-11-04 09:36:48 +0100

    Final submission

--- Point_dendrite/continiously_point_dendrite.py
+++ Point_dendrite/continiously_point_dendrite.py
@@ -1,7 +1,7 @@
 import numpy as np 
 from matplotlib import pyplot as plt 
 #  from channels import *
-from Nassi_channels import *
+from Ionic_channels import *
 import tqdm
 from scipy import stats
 #  plt.style.use('science')
